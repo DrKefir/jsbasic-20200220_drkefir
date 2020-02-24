8f398be9e51d2724f344be199472b54f431cfb73
Jelissej Eirich <jelissej.eirich@gmx.ch>
2020-02-24 15:35:46 +0100

    1-Modul 3-Task reschenie zadachi

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -3,5 +3,10 @@
  * @param {string} str
  * @returns {string}
  */
-function ucFirst(str) {
-}
+ use strict
+  function ucFirst(str) {
+    if (!str) return str;
+    alert( `Perwaja bukwa " ${str[0]} " izmenena v vaschem slowe na zaglawnuju bukwu:\n${str[0].toUpperCase()+str.slice(1)}`);
+    }
+    let str = prompt("Wwedite slowo: ");
+    ucFirst(str);
