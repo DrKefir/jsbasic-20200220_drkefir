649a0547453a3b6dc23c034237a25952d889087b
Jelissej Eirich <jelissej.eirich@gmx.ch>
2020-03-18 18:24:16 +0100

    Усечение строки

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -6,4 +6,9 @@
  */
 function truncate(str, maxlength) {
 
-}
+if (str.length > maxlength) {
+     result = str.slice(0, maxlength - 1) + '…';
+     return result;
+   } else {
+     return str;
+   }
