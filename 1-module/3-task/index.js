99585b13a3c41e12bfa58a0914542936cb99fda4
Jelissej Eirich <jelissej.eirich@gmx.ch>
2020-03-18 14:56:38 +0100

    Zakrit 1-Modul wse zadachi

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -3,9 +3,7 @@
  * @param {string} str
  * @returns {string}
  */
- function ucFirst(str) {
-   if (!str) return str;
-   return str[0].toUpperCase() + str.slice(1);
-   }
-   let str = prompt("Wwedite slowo: ");
-   alert( `Perwaja bukwa izmenena v vaschem slowe na zaglawnuju bukwu:\n${ucFirst(str)}`);
+
+  function ucFirst(str) {
+  return str.slice(0, 1).toUpperCase() + str.slice(1, str.length);
+}
